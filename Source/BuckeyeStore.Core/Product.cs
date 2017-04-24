2c09457b2c06adf0f940d0d8b3c2e66c64219c01
xkong93 <k.xuan29@gmail.com>
2017-04-23 20:40:39 -0400

    finishedpart1

---
+++ Source/BuckeyeStore.Core/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuckeyeStore.Core
+{
+    public class Product
+    {
+        public int Id {get; set;}
+        public string Name {get; set;}
+        public string Description {get; set;}
+        public string Brand {get; set;}
+        public Review Review {get; set;}
+        public AggregateRating AggregateRating {get; set;}
+        public List<Offer> Offers {get; set;}
+        public string sku {get; set;}
+
+        public void AddReview(Review review)
+        {
+            this.Review = review;
+        }
+    }
+}
---
+++ Source/BuckeyeStore.Core/Review.cs
@@ -0,0 +1,10 @@
+
+namespace BuckeyeStore.Core
+{
+    public class Review
+    {
+        public int Id {get; set;}
+        public string Body {get; set;}
+        public Rating Rating {get; set;}
+    }
+}
